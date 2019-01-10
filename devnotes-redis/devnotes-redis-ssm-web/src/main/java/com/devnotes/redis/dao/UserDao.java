eff72469038b3e4a314251805c22ace301ff0304
devnotes <taonyzhang@gmail.com>
2019-01-10 23:49:14 +0800

    init

--- devnotes-redis/devnotes-redis-ssm-web/src/main/java/com/devnotes/redis/dao/UserDao.java
+++ devnotes-redis/devnotes-redis-ssm-web/src/main/java/com/devnotes/redis/dao/UserDao.java
@@ -2,6 +2,7 @@ package com.devnotes.redis.dao;
 
 import com.devnotes.redis.entity.UserDO;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -13,6 +14,7 @@ import java.util.List;
  * @date: 2019-01-10 17:53
  */
 
+@Resource
 public interface UserDao {
 
     List<UserDO> getLists();
